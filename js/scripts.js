f5f31f2c4942a24aff7bc3927ad61d6beeea2933
Andrii-Polo <polovinzik@gmail.com>
2021-04-11 21:42:34 +0300

    dz

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,13 @@ $(document).ready(function() {
        $('.navigation').slideToggle();
        $(this).toggleClass('opened');
     })
+
+    $(function() {
+  
+        $('ul.tabs__caption').on('click', 'li:not(.active)', function() {
+          $(this)
+            .addClass('active').siblings().removeClass('active')
+            .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+        });
+      });
 });
